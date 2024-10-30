ff138321050713512374958b9b94d9b53bed5007
Saurav <epicblueprints@proton.me>
2024-10-30 18:56:23 +0000

    change home page text

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,15 +11,15 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: '~/home',
+    template: '%s | ~/home',
   },
-  description: 'This is my portfolio.',
+  description: 'My digital home.',
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: 'Hello!',
+    description: 'My digital home',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Hello!',
     locale: 'en_US',
     type: 'website',
   },
