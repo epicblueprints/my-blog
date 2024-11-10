c19623b67069d7b6d4713e008e4d1ba4d2dd6bad
Saurav <epicblueprints@proton.me>
2024-11-10 11:49:06 +0000

    update the formatting of twitter link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -30,8 +30,6 @@ export default function Footer() {
             <p className="ml-2 h-7">github</p>
           </a>
         </li>
-      </ul>
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
