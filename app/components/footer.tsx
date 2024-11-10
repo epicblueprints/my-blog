9cbbce2f3b2d71ae1e047c6aa9fb455c87c521cf
Saurav <epicblueprints@proton.me>
2024-11-10 11:38:52 +0000

    Add twitter link and update readme file

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -31,6 +31,19 @@ export default function Footer() {
           </a>
         </li>
       </ul>
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://x.com/epicblueprints"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">twitter</p>
+          </a>
+        </li>
+      </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
         © {new Date().getFullYear()} MIT Licensed
       </p>
